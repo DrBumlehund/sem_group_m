b47c85f07d179113facf642694d7849d2448716e
Thomas August Lemqvist <dr.bumlehund@live.dk>
2017-11-02 11:17:49 +0100

    added an activity for creating an account (no functionality yet)

---
+++ Client/ProjectM/app/src/main/java/m/group/sem/projectm/CreateAccountActivity.java
@@ -0,0 +1,21 @@
+package m.group.sem.projectm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+
+
+    public void createAccount(View view){
+        //TODO: implement a way to create the account
+    }
+
+}
