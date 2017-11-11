df322f016227ff18760d4a481056acb7c0e215cf
Thomas August Lemqvist <dr.bumlehund@live.dk>
2017-11-11 21:52:08 +0100

    added position marker to MainActivity

--- Client/ProjectM/app/src/main/java/m/group/sem/projectm/Activities/MainActivity.java
+++ Client/ProjectM/app/src/main/java/m/group/sem/projectm/Activities/MainActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import Model.User;
 import m.group.sem.projectm.R;
@@ -19,6 +22,14 @@ import m.group.sem.projectm.R;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String tag = "MainActivity";
+
+    // TODO: remove this dummy data, whenever location data is available.
+    private double lat = 55.367397;
+    private double lon = 10.430401;
+
+    // map variables
+    private float zoom;
+
 
     private User mUser;
 
@@ -46,10 +57,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mUser = (User) getIntent().getSerializableExtra("user");
 
         Log.d(tag, "Received user: " + mUser.toString());
+        zoom = 13;
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap map) {
+        LatLng pos = new LatLng(lat, lon);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, zoom));
+        map.addMarker(new MarkerOptions()
+                        .position(pos)
+//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher_foreground))
+                        .draggable(false)
+        );
 
     }
 }
